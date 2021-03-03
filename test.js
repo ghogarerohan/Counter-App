ef6b61fd02008f746df4896f22b60a1f4e100a5b
ghogarerohan <ghogare.rohan@gmail.com>
2021-03-03 22:38:14 +0530

    First Commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// const date1 = new Date('3/12/2020');
+// const date2 = new Date('3/31/2020');
+// const diffTime = Math.abs(date2 - date1);
+// const diffDays = Math.ceil(diffTime); 
+// console.log(diffDays);
+
+let x = [Math.floor((1641590000 / (1000 * 60 * 60)) % 24)].slice(-2)
+
+
+
+console.log(x);
